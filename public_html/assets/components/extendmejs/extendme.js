45fcd8b1af0dbab8e8a1f13c26bffa38d65e7d68
Brayan Rincon <brayan262@gmail.com>
2017-07-27 16:11:39 -0430

    Corrección de dependias bower

---
+++ public_html/assets/components/extendmejs/extendme.js
@@ -0,0 +1,4 @@
+var script = document.createElement('script');
+    script.src = 'dist/extendme.js';
+    script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
